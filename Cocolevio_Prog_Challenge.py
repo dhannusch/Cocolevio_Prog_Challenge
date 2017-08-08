37acddbc953aebed7a771c204b2d985dd25a5fdd
Dennis Hannusch <dennischarleshannusch@gmail.com>
2017-08-08 10:32:26 -0500

    Initial framework idea

---
+++ Cocolevio_Prog_Challenge.py
@@ -0,0 +1,45 @@
+#COCOLEVIO PROGRAMMING CHALLENGE
+#DENNIS HANNUSCH
+
+
+
+# Using class structure to store the data
+class company:
+
+    def __init__(self,companyName,amount,price):
+        self.companyName = name
+        self.amount = amount
+        self.price = price
+        # Calculate utility in order to compare values
+        self.utility = price - amount
+
+
+# Using insertion sort algorithm to sort the data by utility values
+def InsertionSort(companyUtilities):
+
+
+
+
+
+def main():
+
+    # Enter sample data in order to test the code by
+    # creating a list of company class objects
+
+
+    # Enter the total amount of material in supply
+
+
+    # Sort the data by utlity
+
+
+    # Parse through list to find which companies to sell to
+
+
+    # Display the results
+
+
+    
+
+
+main()
